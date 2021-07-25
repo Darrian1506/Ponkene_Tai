c02aa97ba8f731036f55a024177438726d4d6a51
jacaceres <jcaceres@inf.utfsm.cl>
2021-07-25 13:51:00 -0400

    error en reserva.reserva

--- app/Models/Reserva.php
+++ app/Models/Reserva.php
@@ -5,13 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Reserva extends Model
 {
     use HasFactory;
     use Notifiable;
-    use SoftDeletes; 
     protected $table = "reserva";
     protected $primaryKey = "cod_reserva";
     public $incrementing = true;
